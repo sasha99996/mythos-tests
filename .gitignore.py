756d8a7af1684588e70c4be6973b8cceb4e89e0c
Sasha <moanochka123@gmail.com>
2026-05-13 18:41:11 +0300

    Добавлен файл в gitignore

---
+++ .gitignore.py
@@ -0,0 +1,19 @@
+# Allure results (reporting)
+**/allure-results/
+
+# Python cache
+**/__pycache__/
+
+# Pytest cache
+**/.pytest_cache/
+
+# ENV files
+.env
+/venv/
+
+# IDE files
+.idea/
+
+# Other files
+.DS_Store
+.cache
