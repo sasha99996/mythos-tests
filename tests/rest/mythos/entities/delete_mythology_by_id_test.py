d7c7c25cc3cd3c5cfd45d772d7c86d223e25f08e
Sasha <moanochka123@gmail.com>
2026-05-28 18:31:18 +0300

    Исправлены ошибки в автотесте удаления сущности

--- tests/rest/mythos/entities/delete_mythology_by_id_test.py
+++ tests/rest/mythos/entities/delete_mythology_by_id_test.py
@@ -1,6 +1,6 @@
-from clients.rest.mythos_sandbox.entities import create_mythology, delete_mythology_by_id, find_mythology_by_id
+from clients.rest.mythos_sandbox.entities import create_mythology, delete_mythology_by_id, get_mythology_by_id
 from clients.rest.base_auth import get_entities_auth_headers
-from storage.urls import MythosUrls
+
 
 class TestMythology:
     def test_delete_mythology_id(self):
@@ -12,8 +12,8 @@ class TestMythology:
         mythology_id = response.json()["id"]
 
         delete_response = delete_mythology_by_id(auth, mythology_id)
-        assert delete_response.status_code in [200, 204]
+        assert delete_response.status_code == 204, "Сущность не удалена"
 
-        find_response = find_mythology_by_id(auth, mythology_id)
-        assert find_response.status_code == 404
+        find_response = get_mythology_by_id(mythology_id)
+        assert find_response.status_code == 404, "Сущность найдена"
 
