5d05df081f66b7daf9fba440257165542fa31cc9
Sasha <moanochka123@gmail.com>
2026-05-28 18:53:06 +0300

    Исправлены ошибки

--- tests/rest/mythos/entities/put_mythology_id.py
+++ tests/rest/mythos/entities/put_mythology_id.py
@@ -1,17 +1,15 @@
-from clients.rest.mythos_sandbox.entities import create_mythology, delete_mythology_by_id, find_mythology_by_id, fully_update_mythology_by_id, find_mythology_by_id
+from clients.rest.mythos_sandbox.entities import create_mythology, delete_mythology_by_id, fully_update_mythology_by_id, get_mythology_by_id
 from clients.rest.base_auth import get_entities_auth_headers
-from storage.urls import MythosUrls
 
 
 class TestMythology:
     def test_f_update_mythology_by_id(self):
         auth = get_entities_auth_headers()
 
-        response = create_mythology(auth)
+        response = create_mythology(auth, name="Посейдон")
         assert response.status_code == 201, "Сущность не создана"
 
-        data_before = response.json()
-        mythology_id = data_before["id"]
+        mythology_id = response.json()["id"]
 
         update_response = fully_update_mythology_by_id(
             auth,
@@ -20,11 +18,8 @@ class TestMythology:
         )
         assert update_response.status_code == 200, "Персонаж не обновлен"
 
-        get_response = find_mythology_by_id(auth, mythology_id)
-        assert get_response.status_code == 200
-
-        data_after = get_response.json()
-        assert data_after["name"] == "stone"
+        new_name = get_mythology_by_id(mythology_id).json()["name"]
+        assert new_name == "stone", "Имя не изменено"
 
         delete_response = delete_mythology_by_id(auth, mythology_id)
         assert delete_response.status_code == 204, "Персонаж не удален"
