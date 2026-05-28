de07b82f093c022a93cd2ecdeb25e56d769022a9
Sasha <moanochka123@gmail.com>
2026-05-28 19:52:57 +0300

    Добавлены отчеты allure

--- tests/rest/mythos/entities/put_mythology_id.py
+++ tests/rest/mythos/entities/put_mythology_id.py
@@ -1,8 +1,15 @@
+import allure
+import pytest
+
 from clients.rest.mythos_sandbox.entities import create_mythology, delete_mythology_by_id, fully_update_mythology_by_id, get_mythology_by_id
 from clients.rest.base_auth import get_entities_auth_headers
 
 
+@pytest.mark.put_mythology
+@allure.feature("PUT /api/mythology")
 class TestMythology:
+    @allure.testcase("https://app.qase.io/case/MSA-22")
+    @allure.title("Полное обновление сущности")
     def test_f_update_mythology_by_id(self):
         auth = get_entities_auth_headers()
 
@@ -24,14 +31,17 @@ class TestMythology:
         delete_response = delete_mythology_by_id(auth, mythology_id)
         assert delete_response.status_code == 204, "Персонаж не удален"
 
-
+    @allure.testcase("https://app.qase.io/case/MSA-22")
+    @allure.title("Полное обновление сущности через фикстуру")
     def test_f_update_mythology_by_id_with_fixtures(self, auth_user_tuco, mythology_id_by_user_tuco):
-        update_response = fully_update_mythology_by_id(
-            auth_user_tuco,
-            mythology_id_by_user_tuco,
-            name="stone"
-        )
-        assert update_response.status_code == 200, "Персонаж не обновлен"
-
-        new_name = get_mythology_by_id(mythology_id_by_user_tuco).json()["name"]
-        assert new_name == "stone", "Имя не изменено"
+        with allure.step("Шаг: Обновить сущность"):
+            update_response = fully_update_mythology_by_id(
+                auth_user_tuco,
+                mythology_id_by_user_tuco,
+                name="stone"
+            )
+        with allure.step("Проверка: Код ответа равен 200"):
+            assert update_response.status_code == 200, "Персонаж не обновлен"
+        with allure.step("Проверка: Имя обновлено"):
+            new_name = get_mythology_by_id(mythology_id_by_user_tuco).json()["name"]
+            assert new_name == "stone", "Имя не изменено"
