3a8da16efff9e1049f46dc50241e49e71793e559
Sasha <moanochka123@gmail.com>
2026-06-06 22:45:28 +0300

    Добавлен автотест на сортировку с параметризацией (еще буду редактировать)

--- tests/rest/mythos/entities/get_mythology_test.py
+++ tests/rest/mythos/entities/get_mythology_test.py
@@ -2,7 +2,7 @@ import pytest
 from clients.rest.base_http import send_request
 from storage.urls import MythosUrls
 from clients.rest.mythos_sandbox.entities import get_all_mythology
-from clients.rest.base_validation import check_response_code, check_value_in_dicts
+from clients.rest.base_validation import check_response_code, check_value_in_dicts, check_dicts_are_sorted_by_key
 
 
 
@@ -17,4 +17,11 @@ class TestMythology:
         response = get_all_mythology(params={"category": category})
         check_response_code(response, expected_code=200)
         check_value_in_dicts(response.json(), key="category", value=category)
+
+
+    @pytest.mark.parametrize("sort", ["asc", "desc"])
+    def test_get_mythology__list_sorted_by_key(self, sort):
+        response = get_all_mythology(params={"sort": sort})
+        check_response_code(response, expected_code=200)
+        check_dicts_are_sorted_by_key(response.json(), key="id", reverse=(sort == "desc"))
 
