cd87659d366e6b134aaf7eb38faf84b160445c37
Sasha <moanochka123@gmail.com>
2026-06-14 19:39:13 +0300

    Добавлен положительный автотест для метода patch

--- tests/rest/mythos/entities/patch_mythology_id.py
+++ tests/rest/mythos/entities/patch_mythology_id.py
@@ -1,12 +1,21 @@
 import allure
-import pytest
-from clients.rest.mythos_sandbox.entities import create_mythology, delete_mythology_by_id, fully_update_mythology_by_id, get_mythology_by_id, partial_update_mythology_by_id
+from clients.rest.mythos_sandbox.entities import partial_update_mythology_by_id
 from clients.rest.base_auth import get_entities_auth_headers
 from clients.rest.base_validation import check_response_code
 
 
 @allure.feature("PATCH /api/mythology")
 @allure.title("Частичное обновление сущности")
+def test_patch_mythology_without_auth(mythology_id_by_user_tuco):
+    response = partial_update_mythology_by_id(
+        auth=get_entities_auth_headers(),
+        mythology_id=mythology_id_by_user_tuco,
+        name="Зевс")
+    check_response_code(response, expected_code=201)
+
+
+@allure.feature("PATCH /api/mythology")
+@allure.title("Частичное обновление сущности (негативный тест)")
 def test_patch_mythology_without_auth(mythology_id_by_user_tuco):
     response = partial_update_mythology_by_id(
         auth=None,
