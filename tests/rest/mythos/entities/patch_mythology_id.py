7928ffb3c6a8b3aae623920cedccfa1991748d4d
Sasha <moanochka123@gmail.com>
2026-06-15 13:58:09 +0300

    внесены правки

--- tests/rest/mythos/entities/patch_mythology_id.py
+++ tests/rest/mythos/entities/patch_mythology_id.py
@@ -1,17 +1,20 @@
 import allure
-from clients.rest.mythos_sandbox.entities import partial_update_mythology_by_id
+from clients.rest.mythos_sandbox.entities import partial_update_mythology_by_id, get_mythology_by_id
 from clients.rest.base_auth import get_entities_auth_headers
 from clients.rest.base_validation import check_response_code
 
 
 @allure.feature("PATCH /api/mythology")
 @allure.title("Частичное обновление сущности")
-def test_patch_mythology_without_auth(mythology_id_by_user_tuco):
+def test_patch_mythology_with_auth(auth_user_tuco, mythology_id_by_user_tuco):
     response = partial_update_mythology_by_id(
-        auth=get_entities_auth_headers(),
+        auth=auth_user_tuco,
         mythology_id=mythology_id_by_user_tuco,
         name="Зевс")
-    check_response_code(response, expected_code=201)
+    check_response_code(response, expected_code=200)
+    get_response = get_mythology_by_id(mythology_id_by_user_tuco)
+    check_response_code(get_response, 200)
+    assert get_response.json()["name"] == "Зевс"
 
 
 @allure.feature("PATCH /api/mythology")
