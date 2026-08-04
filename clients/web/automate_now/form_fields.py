9c6fad79677badb7291d2b6ee1e796bc03925ccb
Sasha <moanochka123@gmail.com>
2026-08-04 12:53:46 +0300

    Дописаны функции и автотест Form Fields

--- clients/web/automate_now/form_fields.py
+++ clients/web/automate_now/form_fields.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from clients.web.base_page import BasePage
 
+
 class FormFields(BasePage):
     NAME_FIELD = (
         By.CSS_SELECTOR,
@@ -48,5 +49,45 @@ class FormFields(BasePage):
         fill_in_name_field.click()
         fill_in_name_field.send_keys(name)
 
-    
+    def fill_in_password_field(self, browser, name='test123'):
+        """Заполняет поле 'Name' на веб-странице"""
+        fill_in_password_field = self.find_element(browser, *self.PASSWORD_FIELD, time_wait=20)
+        fill_in_password_field.click()
+        fill_in_password_field.send_keys(name)
+
+    def choose_favorite_drink(self, browser, name='Milk'):
+        """Выбирает поле 'Favorite Drink' на веб-странице"""
+        choose_favorite_drink = self.find_element(browser, *self.FAVORITE_DRINK_MILK, time_wait=20)
+        choose_favorite_drink.click()
+        choose_favorite_drink.send_keys(name)
+
+    def choose_favorite_color(self, browser, name='Yellow'):
+        """Выбирает поле 'Favorite Color' на веб-странице"""
+        choose_favorite_color = self.find_element(browser, *self.FAVORITE_COLOR_YELLOW, time_wait=20)
+        choose_favorite_color.click()
+        choose_favorite_color.send_keys(name)
+
+    def choose_yes(self, browser, name='Yes'):
+        """Выбирает опцию 'Yes' на веб странице в опросе 'Do you like automation?'"""
+        choose_yes = self.find_element(browser, *self.AUTOMATION_FIELD_YES, time_wait=20)
+        choose_yes.click()
+        choose_yes.send_keys(name)
+
+    def fill_in_email(self, browser, name='ivan@gmail.com'):
+        """Заполняет поле 'Email' на веб-странице"""
+        fill_in_email = self.find_element(browser, *self.EMAIL_FIELD_IVAN, time_wait=20)
+        fill_in_email.click()
+        fill_in_email.send_keys(name)
+
+    def fill_in_message(self, browser, name='ыыы ааа'):
+        """Заполняет поле 'Message' на веб-странице"""
+        fill_in_message = self.find_element(browser, *self.MESSAGE_FIELD, time_wait=20)
+        fill_in_message.click()
+        fill_in_message.send_keys(name)
+
+    def click_btn_submit(self, browser, name='submit'):
+        """Нажимает кнопку 'Submit' на веб-странице"""
+        click_btn_submit = self.find_element(browser, *self.BTN_SUBMIT, time_wait=20)
+        click_btn_submit.click()
+        click_btn_submit.send_keys(name)
 
--- tests/web/automate_now/form_field_test.py
+++ tests/web/automate_now/form_field_test.py
@@ -1,6 +1,9 @@
 from clients.web.automate_now.form_fields import FormFields
+from selenium.webdriver.common.by import By
 import pytest
 import allure
+from storage.urls import AutomateNow
+from selenium import webdriver
 
 @pytest.mark.automate_now_form_fields
 @allure.feature("Form_Fields")
@@ -8,5 +11,23 @@ class TestAutomateNowFormFields:
     @allure.testcase("https://app.qase.io/case/AN-12")
     @allure.title("Заполнение обязательных полей Form Fields")
     def test_fill_in_fields(self, browser):
+        with allure.step("Шаг: открываем главную страницу"):
+            browser.get(AutomateNow.BASE_URL)
+        with allure.step("Шаг: нажимаем кнопку  'Form Fields'"):
+            browser.find_element(By.LINK_TEXT, "Form Fields").click()
         with allure.step("Шаг: Заполняем поле 'Name'"):
             FormFields().fill_in_name_field(browser)
+        with allure.step("Шаг: Заполняем поле 'Password'"):
+            FormFields().fill_in_password_field(browser)
+        with allure.step("Шаг: Выбираем 'Milk' в опроснике 'Favorite Drink"):
+            FormFields().choose_favorite_drink(browser)
+        with allure.step("Шаг: Выбираем 'Yellow' в опроснике 'Favorite Color"):
+            FormFields().choose_favorite_color(browser)
+        with allure.step("Шаг: Выбираем 'Yes' в опроснике 'Do you like automation?'"):
+            FormFields().choose_yes(browser)
+        with allure.step("Шаг: Вводим email 'ivan@gmail.com' в поле 'Email'"):
+            FormFields().fill_in_email(browser)
+        with allure.step("Шаг: Вводим сообщение 'ыыы ааа' в поле 'Message'"):
+            FormFields().fill_in_message(browser)
+        with allure.step("Шаг: Нажать кнопку 'Submit' на веб-странице"):
+            FormFields().click_btn_submit(browser)
