ce741abe9552ce6f0128c64706a283c4124f6d86
Sasha <moanochka123@gmail.com>
2026-08-15 11:47:58 +0300

    Исправлены ошибки в автотесте и файле form_fields

--- clients/web/automate_now/form_fields.py
+++ clients/web/automate_now/form_fields.py
@@ -10,84 +10,160 @@ class FormFields(BasePage):
 
     PASSWORD_FIELD = (
         By.CSS_SELECTOR,
-        "input[type='password'",
+        "input[type='password']",
         "Поле 'Password' на веб-странице")
 
     FAVORITE_DRINK_MILK = (
         By.CSS_SELECTOR,
         "#drink2",
-        "Выбрать  'Milk' в качестве любимого напитка")
+        "Чек-бокс 'Milk' для поля 'What is your favorite drink?'")
+
+    FAVORITE_DRINK_WATER = (
+        By.CSS_SELECTOR,
+        "#drink1",
+        "Чек-бокс 'Water' для поля 'What is your favorite drink?'")
+
+    FAVORITE_DRINK_COFFEE = (
+        By.CSS_SELECTOR,
+        "#drink3",
+        "Чек-бокс 'Coffee' для поля 'What is your favorite drink?'")
 
     FAVORITE_COLOR_YELLOW = (
         By.CSS_SELECTOR,
         "#color3",
-        "Выбрать 'Yellow' в качестве любимого цвета")
+        "Чек-бокс 'Yellow' для поля 'Favorite Color'")
+
+    FAVORITE_COLOR_RED = (
+        By.CSS_SELECTOR,
+        "#color1",
+        "Чек-бокс 'Red' для поля 'Favorite Color'")
+
+    FAVORITE_COLOR_BLUE = (
+        By.CSS_SELECTOR,
+        "#color2",
+        "Чек-бокс 'Blue' для поля 'Favorite Color'")
+
+    FAVORITE_COLOR_GREEN = (
+        By.CSS_SELECTOR,
+        "#color4",
+        "Чек-бокс 'Green' для поля 'Favorite Color'")
+
+    FAVORITE_COLOR_FFC0CB = (
+        By.CSS_SELECTOR,
+        "#color5",
+        "Чек-бокс '#FFC0CB' для поля 'Favorite Color'")
 
     AUTOMATION_FIELD_YES = (
         By.CSS_SELECTOR,
         "[data-testid='automation-yes']",
-        "Выбрать 'Yes' в опроснике")
+        "Чек-бокс 'Yes' для поля 'Do you like automation?'")
 
-    EMAIL_FIELD_IVAN = (
+    AUTOMATION_FIELD_NO = (
+        By.CSS_SELECTOR,
+        "[data-testid='automation-no']",
+        "Чек-бокс 'No' для поля 'Do you like automation?'")
+
+    AUTOMATION_FIELD_UNDECIDED = (
+        By.CSS_SELECTOR,
+        "[data-testid='automation-undecided']",
+        "Чек-бокс 'Undecided' для поля 'Do you like automation?'")
+
+    EMAIL_FIELD = (
         By.CSS_SELECTOR,
         "#email",
-        "Ввести 'ivan@gmail.com' в поле email")
+        "Поле 'Email' на веб-странице")
 
     MESSAGE_FIELD = (
         By.CSS_SELECTOR,
         "#message",
-        "Ввести сообщение 'ыыы ааа'")
+        "Поле 'Message' на веб-странице")
 
     BTN_SUBMIT = (
         By.CSS_SELECTOR,
         "#submit-btn",
-        "Нажать кнопку 'Submit'")
+        "Кнопка 'Submit' на веб-странице")
 
-    def fill_in_name_field(self, browser, name='Ivan'):
-        """Заполняет поле 'Name' на веб-странице"""
-        fill_in_name_field = self.find_element(browser, *self.NAME_FIELD, time_wait=20)
-        fill_in_name_field.click()
-        fill_in_name_field.send_keys(name)
+    def fill_in_name_field(self, browser, name="Ivan"):
+        """Заполняет поле 'Name' на веб-странице."""
+        name_field = self.find_element(
+            browser, *self.NAME_FIELD, time_wait=20
+        )
+        name_field.click()
+        name_field.send_keys(name)
 
-    def fill_in_password_field(self, browser, name='test123'):
-        """Заполняет поле 'Name' на веб-странице"""
-        fill_in_password_field = self.find_element(browser, *self.PASSWORD_FIELD, time_wait=20)
-        fill_in_password_field.click()
-        fill_in_password_field.send_keys(name)
+    def fill_in_password_field(self, browser, password="test123"):
+        """Заполняет поле 'Password' на веб-странице."""
+        password_field = self.find_element(
+            browser, *self.PASSWORD_FIELD, time_wait=20
+        )
+        password_field.click()
+        password_field.send_keys(password)
 
-    def choose_favorite_drink(self, browser, name='Milk'):
-        """Выбирает поле 'Favorite Drink' на веб-странице"""
-        choose_favorite_drink = self.find_element(browser, *self.FAVORITE_DRINK_MILK, time_wait=20)
-        choose_favorite_drink.click()
-        choose_favorite_drink.send_keys(name)
+    def choose_favorite_drink(self, browser, favorite_drink="Milk"):
+        """Выбирает любимый напиток на веб-странице."""
+        drinks = {
+            "Milk": self.FAVORITE_DRINK_MILK,
+            "Water": self.FAVORITE_DRINK_WATER,
+            "Coffee": self.FAVORITE_DRINK_COFFEE,
+        }
+        favorite_drink_field = self.find_element(
+            browser, *drinks[favorite_drink], time_wait=20
+        )
+        favorite_drink_field.click()
 
-    def choose_favorite_color(self, browser, name='Yellow'):
-        """Выбирает поле 'Favorite Color' на веб-странице"""
-        choose_favorite_color = self.find_element(browser, *self.FAVORITE_COLOR_YELLOW, time_wait=20)
-        choose_favorite_color.click()
-        choose_favorite_color.send_keys(name)
+    def choose_favorite_color(self, browser, favorite_color="Yellow"):
+        """Выбирает любимый цвет на веб-странице."""
+        colors = {
+            "Red": self.FAVORITE_COLOR_RED,
+            "Blue": self.FAVORITE_COLOR_BLUE,
+            "Yellow": self.FAVORITE_COLOR_YELLOW,
+            "Green": self.FAVORITE_COLOR_GREEN,
+            "#FFC0CB": self.FAVORITE_COLOR_FFC0CB,
+        }
+        favorite_color_field = self.find_element(
+            browser, *colors[favorite_color], time_wait=20
+        )
+        favorite_color_field.click()
 
-    def choose_yes(self, browser, name='Yes'):
-        """Выбирает опцию 'Yes' на веб странице в опросе 'Do you like automation?'"""
-        choose_yes = self.find_element(browser, *self.AUTOMATION_FIELD_YES, time_wait=20)
-        choose_yes.click()
-        choose_yes.send_keys(name)
+    def choose_automation_answer(self, browser, answer="Yes"):
+        """Выбирает вариант ответа для поля 'Do you like automation?'."""
+        answers = {
+            "Yes": self.AUTOMATION_FIELD_YES,
+            "No": self.AUTOMATION_FIELD_NO,
+            "Undecided": self.AUTOMATION_FIELD_UNDECIDED,
+        }
+        automation_answer_field = self.find_element(
+            browser, *answers[answer], time_wait=20
+        )
+        automation_answer_field.click()
 
-    def fill_in_email(self, browser, name='ivan@gmail.com'):
-        """Заполняет поле 'Email' на веб-странице"""
-        fill_in_email = self.find_element(browser, *self.EMAIL_FIELD_IVAN, time_wait=20)
-        fill_in_email.click()
-        fill_in_email.send_keys(name)
+    def fill_in_email(self, browser, email="ivan@gmail.com"):
+        """Заполняет поле 'Email' на веб-странице."""
+        email_field = self.find_element(
+            browser, *self.EMAIL_FIELD, time_wait=20
+        )
+        email_field.click()
+        email_field.send_keys(email)
 
-    def fill_in_message(self, browser, name='ыыы ааа'):
-        """Заполняет поле 'Message' на веб-странице"""
-        fill_in_message = self.find_element(browser, *self.MESSAGE_FIELD, time_wait=20)
-        fill_in_message.click()
-        fill_in_message.send_keys(name)
+    def fill_in_message(self, browser, message="ыыы ааа"):
+        """Заполняет поле 'Message' на веб-странице."""
+        message_field = self.find_element(
+            browser, *self.MESSAGE_FIELD, time_wait=20
+        )
+        message_field.click()
+        message_field.send_keys(message)
 
-    def click_btn_submit(self, browser, name='submit'):
-        """Нажимает кнопку 'Submit' на веб-странице"""
-        click_btn_submit = self.find_element(browser, *self.BTN_SUBMIT, time_wait=20)
-        click_btn_submit.click()
-        click_btn_submit.send_keys(name)
+    def click_btn_submit(self, browser):
+        """Нажимает кнопку 'Submit' на веб-странице."""
+        submit_button = self.find_element(
+            browser, *self.BTN_SUBMIT, time_wait=20
+        )
+        submit_button.click()
+
+    def get_success_message(self, browser):
+        """Возвращает сообщение об успешной отправке формы."""
+        alert = browser.switch_to.alert
+        message = alert.text
+        alert.accept()
+        return message
 
--- tests/web/automate_now/form_field_test.py
+++ tests/web/automate_now/form_field_test.py
@@ -1,9 +1,7 @@
 from clients.web.automate_now.form_fields import FormFields
-from selenium.webdriver.common.by import By
 import pytest
 import allure
 from storage.urls import AutomateNow
-from selenium import webdriver
 
 @pytest.mark.automate_now_form_fields
 @allure.feature("Form_Fields")
@@ -11,23 +9,24 @@ class TestAutomateNowFormFields:
     @allure.testcase("https://app.qase.io/case/AN-12")
     @allure.title("Заполнение обязательных полей Form Fields")
     def test_fill_in_fields(self, browser):
-        with allure.step("Шаг: открываем главную страницу"):
-            browser.get(AutomateNow.BASE_URL)
-        with allure.step("Шаг: нажимаем кнопку  'Form Fields'"):
-            browser.find_element(By.LINK_TEXT, "Form Fields").click()
+        with allure.step("Шаг: открываем  страницу Form Fields"):
+            browser.get(AutomateNow.FORM_FIELDS_URL)
         with allure.step("Шаг: Заполняем поле 'Name'"):
             FormFields().fill_in_name_field(browser)
         with allure.step("Шаг: Заполняем поле 'Password'"):
             FormFields().fill_in_password_field(browser)
-        with allure.step("Шаг: Выбираем 'Milk' в опроснике 'Favorite Drink"):
+        with allure.step("Выбираем чек-бокс 'Milk' для поля 'What is your favorite drink?'"):
             FormFields().choose_favorite_drink(browser)
         with allure.step("Шаг: Выбираем 'Yellow' в опроснике 'Favorite Color"):
             FormFields().choose_favorite_color(browser)
         with allure.step("Шаг: Выбираем 'Yes' в опроснике 'Do you like automation?'"):
-            FormFields().choose_yes(browser)
+            FormFields().choose_automation_answer(browser)
         with allure.step("Шаг: Вводим email 'ivan@gmail.com' в поле 'Email'"):
             FormFields().fill_in_email(browser)
         with allure.step("Шаг: Вводим сообщение 'ыыы ааа' в поле 'Message'"):
             FormFields().fill_in_message(browser)
-        with allure.step("Шаг: Нажать кнопку 'Submit' на веб-странице"):
+        with allure.step("Шаг: Нажимаем кнопку 'Submit' на веб-странице"):
             FormFields().click_btn_submit(browser)
+        with allure.step("Шаг: Проверяем сообщение об успешной отправке"):
+            success_message = FormFields().get_success_message(browser)
+            assert success_message == "Message received!"
