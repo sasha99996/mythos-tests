29fd19b9da50d3a52fe88c5a21be6f03b92369d5
Sasha <moanochka123@gmail.com>
2026-08-19 16:07:04 +0300

    Добавлен автотест с параметризацией

--- tests/web/automate_now/form_field_test.py
+++ tests/web/automate_now/form_field_test.py
@@ -34,3 +34,20 @@ class TestAutomateNowFormFields:
         with allure.step("Шаг: Проверяем сообщение об успешной отправке"):
             success_message = FormFields().get_success_message(browser)
             assert success_message == "Message received!"
+
+
+    @pytest.mark.parametrize("answer_param", ["Yes", "No", "Undecided"])
+    @allure.testcase("https://app.qase.io/case/AN-15")
+    @allure.title("Автотест с параметризацией в опроснике 'Do you like automation?'")
+    def test_automation_with_param (self, answer_param, browser):
+        with allure.step("Шаг: открываем  страницу Form Fields"):
+            browser.get(AutomateNow.FORM_FIELDS_URL)
+        with allure.step("Шаг: Заполняем поле 'Name'"):
+            FormFields().fill_in_name_field(browser)
+        with allure.step("Шаг: Выбираем один из 3 вариантов ответа в опроснике 'Do you like automation?'"):
+            FormFields().choose_automation_answer(browser, answer=answer_param)
+        with allure.step("Шаг: Нажимаем кнопку 'Submit' на веб-странице"):
+            FormFields().click_btn_submit(browser)
+        with allure.step("Шаг: Проверяем сообщение об успешной отправке"):
+            success_message = FormFields().get_success_message(browser)
+            assert success_message == "Message received!"
